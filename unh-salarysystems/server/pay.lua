67f685bb1218c76098ef5aeb84c15c426ccea235
Brillanc3 <45542413+Brillanc3@users.noreply.github.com>
2022-03-10 20:36:18 +0100

    reset cumultative_salary to 0

--- unh-salarysystems/server/pay.lua
+++ unh-salarysystems/server/pay.lua
@@ -1,6 +1,8 @@
 paytime = function(source)
     local _source = source
+    local license = getLicense(_source)
     local xPlayer = ESX.GetPlayerFromId(_source)
     local total_salary = cumultative_salary(_source) + xPlayer.getJob().grade_salary
     xPlayer.addAccountMoney('bank', total_salary)
+    MySQL.scalar.await('UPDATE cumultative_salary = ? FROM salary WHERE identifier = ?', {0, license})
 end
