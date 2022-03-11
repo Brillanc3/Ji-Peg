ad8687384bda772d330cb06ed5001ee468363c16
Brillanc3 <45542413+Brillanc3@users.noreply.github.com>
2022-03-11 13:35:08 +0100

    update

--- unh-salarysystems/client/cmain.lua
+++ unh-salarysystems/client/cmain.lua
@@ -3,33 +3,36 @@ local loop, connected = false, false
 local playerLicense = ""
 local needtime = nil
 
-Citizen.CreateThread(function()
-    while true do
-        while playerLicense == "" do
-            ESX.TriggerServerCallback('esx:getPlayerData', function(data)
-                playerLicense = "license:" .. data.identifier
-                connected = true
-            end)
-            Citizen.Wait(0)
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+    Citizen.CreateThread(function()
+        while true do
+            while playerLicense == "" do
+                ESX.TriggerServerCallback('esx:getPlayerData', function(data)
+                    playerLicense = "license:" .. data.identifier
+                    connected = true
+                end)
+                Citizen.Wait(0)
+            end
+            if connected then
+                TriggerServerEvent("unh-salarysystems:connected", playerLicense)
+                connected = not connected
+            end
+            if needtime == nil or needtime <= 0 then
+                ESX.TriggerServerCallback("unh-salary:getSalaryData", function(data)
+                    while needtime == nil do
+                        needtime = data
+                        Citizen.Wait(0)
+                    end
+                end)
+            end
+            Citizen.Wait(1000)
+            needtime = needtime - 1
+            print("NeedTime :", needtime)
+            if needtime <= 0 then
+                TriggerServerEvent("unh-salarysystems:update", playerLicense)
+                needtime = 3600
+            end
         end
-        if connected then
-            TriggerServerEvent("unh-salarysystems:connected", playerLicense)
-            connected = not connected
-        end
-        if needtime == nil or needtime <= 0 then
-            ESX.TriggerServerCallback("unh-salary:getSalaryData", function(data)
-                while needtime == nil do
-                    needtime = data
-                    Citizen.Wait(0)
-                end
-            end)
-        end
-        Citizen.Wait(1000)
-        needtime = needtime - 1
-        print("NeedTime :", needtime)
-        if needtime <= 0 then
-            TriggerServerEvent("unh-salarysystems:update", playerLicense)
-            needtime = 3600
-        end
-    end
+    end)
 end)
